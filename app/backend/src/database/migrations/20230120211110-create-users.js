dc09d3b67b0d605f1bd9ae87bb0e983e805d9868
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-01-20 23:28:33 +0100

    tentando exercicio 1 com ajuda

---
+++ app/backend/src/database/migrations/20230120211110-create-users.js
@@ -0,0 +1,34 @@
+ "use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const UsersTable = queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+
+    return UsersTable;
+  },
+
+  down: async (queryInterface) => queryInterface.dropTable("users"),
+};
---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,25 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  username: DataTypes.STRING,
+  role: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
